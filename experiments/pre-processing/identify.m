3c13129f43dd9b264fca0beae4d66accb3d67c00
hmph <anurag.arnab@gmail.com>
2014-07-29 12:34:58 +0200

    So I have started testing for my ROC. Threshold value of 6 gives approximately FAR and FRR of 5% which I think is ok (this was with those outliers removed). I wrote some notes in my notebook, since my FAR includes incorrectly classified labels. But this will take a very long time to run

--- experiments/pre-processing/identify.m
+++ experiments/pre-processing/identify.m
@@ -1,14 +1,30 @@
-function [ output_id ] = identify( chosen, hd_fraction, veins, threshold )
+function [ output_id ] = identify( chosen, hd_fraction, veins, threshold, is_imposter_test, veins_imposter, verbose )
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
-    [id distance ~] = test_identity (chosen, hd_fraction, veins);
+    if (nargin < 5)
+       verbose = 0; 
+       is_imposter_test = 0;
+    elseif (nargin < 7)
+        verbose = 0;
+    end
     
+    if (~is_imposter_test)
+        [id distance ~] = test_identity (chosen, hd_fraction, veins);
+    else
+        [id distance ~] = test_identity_2 (veins_imposter(chosen), hd_fraction, veins);
+    end
     if (distance < threshold)
-       fprintf ('Identified as %i since distance was %0.3f', id, distance);
+        
+       if (verbose) 
+            fprintf ('Identified as %i since distance was %0.3f', id, distance);
+       end
        output_id = id;
     else
-       fprintf ('Rejected. Distance to %i swas %0.3f', id, distance);
+        
+       if (verbose)
+           fprintf ('Rejected. Distance to %i swas %0.3f', id, distance);
+       end
        output_id = -1; 
     end
 
---
+++ experiments/pre-processing/unit_test_mhd_for_roc.m
@@ -0,0 +1,79 @@
+%incorrect = 0;
+%outlier_count = 0; % I am going to count errors on images 4, 5 and 6 after the 40th hand as an outlier
+%threshold = 6;
+
+thresholds = [1 2 4 5 5.2 5.4 5.6 5.8 6 6.2 6.4 6.6 6.8 7 8];
+
+correct_record = zeros (length(thresholds));
+correct_neg_record = zeros (length(thresholds));
+false_acceptance_record = zeros (length(thresholds));
+outlier_far_record = zeros (length(thresholds));
+false_rejection_record = zeros (length(thresholds));
+outlier_frr_record = zeros (length(thresholds));
+
+for k = 1:length(thresholds)
+
+    %only_test_imposters = 1;
+    
+    %if (only_test_imposters)
+    threshold = thresholds (k);
+    
+    correct = 0;
+    correct_neg = 0;
+    false_acceptance = 0;
+    outlier_far = 0;
+    false_rejection = 0;
+    outlier_frr = 0;
+    
+    for j = 1:length(veins)
+
+       output_id = identify (j, 0.8, veins, threshold);
+       [start finish] = get_limits (j, 6);
+       index = mod (j, 6);
+
+
+       if (output_id == -1)
+          % Identifier is saying that the person does not exist in the system,
+          % but we know that he does!
+          false_rejection = false_rejection + 1; 
+
+          if ( (index == 0 || index == 4 || index == 5) && j > 240)
+               outlier_frr = outlier_frr + 1;
+          end
+
+       elseif (output_id < start || output_id > finish)
+          % Identifier has identified the wrong person
+          false_acceptance = false_acceptance + 1;
+
+          if ( (index == 0 || index == 4 || index == 5) && j > 240)
+               outlier_far = outlier_far + 1;
+          end
+
+       else
+           correct = correct + 1;
+       end
+
+    end
+
+    %end
+
+    for j=1:length(veins)
+
+        output_id = identify (j, 0.8, veins, threshold, 1, veins_unreg);
+        if (output_id ~= -1)
+           false_acceptance = false_acceptance + 1;
+           fprintf ('Error with imposter %i, detected as %i\n', j, output_id);
+        else
+            correct_neg = correct_neg + 1;
+        end
+
+    end
+    
+    fprintf ('Completed for threshold %0.2f\n', threshold);
+    correct_record(k) = correct;
+    correct_neg_record(k) = correct_neg;
+    false_acceptance_record(k) = false_acceptance;
+    outlier_far_record(k) = outlier_far;
+    false_rejection_record(k) = false_rejection;
+    outlier_frr_record(k) = outlier_frr;
+end
