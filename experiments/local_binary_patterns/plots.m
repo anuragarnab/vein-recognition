132ea38216d70d5ad354641088913b3821a41c2d
hmph <anurag.arnab@gmail.com>
2014-08-18 18:00:49 +0200

    Made proper testing code. Got an ROC curve. Should account for outliers to properly compare with the mhd

---
+++ experiments/local_binary_patterns/plots.m
@@ -0,0 +1,25 @@
+% putting t as thresh
+%t = [100, 150, 200];
+thresh = [1 2:0.1:3 3.5 4 5];
+
+figure
+plot (thresh, sum(correct_record) ./ length(veins) * 100, 'blue')
+hold on
+plot (thresh, sum(correct_neg_record) ./ length(veins_unreg) * 100, 'red')
+legend ('Genuine samples correctly accepted', 'Imposter samples correctly rejected')
+
+figure
+%plot (far_record./600 .* 100 , frr_record./200 .* 100);
+%xlabel ('False acceptance rate')
+%ylabel ('False rejection rate');
+
+far = sum(far_record) + sum(wrong_rec_record);
+denom = length(veins_unreg) + sum(wrong_rec_record);
+
+plot (sum(frr_record)./length(veins) .* 100, far./denom .* 100);
+ylabel ('False acceptance rate')
+xlabel ('False rejection rate');
+x = ones(100,1);
+x = cumsum(x);
+hold on
+plot(x,x, 'green')
---
+++ experiments/local_binary_patterns/test_for_roc.m
@@ -0,0 +1,62 @@
+load veins
+load veins_unreg
+
+samples_per_person = 6;
+
+correct_record = [];
+correct_neg_record = [];
+far_record = [];
+frr_record = [];
+wrong_rec_record = [];
+
+for threshold = [1 2:0.1:3 3.5 4 5]
+
+    fprintf('Testing threshold = %0.3f\n', threshold);
+    % These four effectively make up the quadrant
+    correct = zeros (length(veins),1);
+    correct_neg = zeros (length(veins_unreg),1);
+    far = zeros (length(veins_unreg),1);
+    frr = zeros (length(veins),1);
+
+    wrong_recognised = zeros(length(veins),1); % Special case of far I guess
+
+    for i = 1:length(veins)
+        id = identify( veins(i), veins, threshold, 1, i); % The "1" is saying that we are doing LOO testing
+
+        [start, finish] = get_limits(i, samples_per_person);
+        if (id > start && id <= finish)
+            correct(i) = 1;
+        elseif (id == -1)
+            frr(i) = 1;
+        else
+            wrong_recognised(i) = 1;
+        end 
+
+        if (mod(i,100) == 0)
+           fprintf('%i\n', i); 
+        end
+    end
+
+    for i=1:length(veins_unreg)
+       id = identify(veins_unreg(i), veins, threshold); 
+
+       if (id == -1)
+          correct_neg(i) = 1;
+       else
+           far(i) = 1;
+       end
+
+       if (mod(i,100) == 0)
+           fprintf('%i\n', i); 
+       end
+    end
+    
+    correct_record = [correct_record correct];
+    correct_neg_record = [correct_neg_record correct_neg];
+    far_record = [far_record far];
+    frr_record = [frr_record frr];
+    wrong_rec_record = [wrong_rec_record wrong_recognised];
+    
+    save lbp_test threshold correct_record correct_neg_record far_record frr_record wrong_rec_record
+
+end
