082430c83b91eecfdfea0ce83a9d7611ceab6b75
hmph <anurag.arnab@gmail.com>
2014-08-23 14:16:47 +0200

    Added the histogrammed version of ulbp. Changed ChiSquare distance to handle matrix of histogram distances. Needs to be tested

---
+++ experiments/local_binary_patterns/grid_histogram.m
@@ -0,0 +1,37 @@
+function [ histograms ] = grid_histogram( image, r, c, histogram_range )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+    [height, width] = size(image);
+    histograms = [];
+    
+    if (nargin < 4)
+       histogram_range = [0:1:58];
+    end
+    
+    rows = zeros(r,2);
+    cols = zeros(r,2);
+    
+    for i = 1:r
+        rows(i,1) = floor(height*(i-1)/r)+1;
+        rows(i,2) = floor(height*i/r);
+    end
+    
+    for i = 1:c
+       cols(i,1) = floor(width*(i-1)/c)+1;
+       cols(i,2) = floor(width*i/c);
+    end
+    
+    %count = 1;
+    for ir = 1:size(rows,1)
+       for ic = 1:size(cols,1)
+          im = image(rows(ir,1):rows(ir,2) , cols(ic,1):cols(ic,2) );
+          %subplot(r,c,count);
+          %imshow(im);
+          h = hist(im(:),[0:1:58]);
+          histograms = [histograms ; h];
+          %count = count+1;
+       end
+    end
+
+end
+
