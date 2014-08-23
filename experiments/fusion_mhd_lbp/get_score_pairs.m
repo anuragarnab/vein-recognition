b5be4b2adcd7cf0473bda4d73cf4a3cb083acfeb
hmph <anurag.arnab@gmail.com>
2014-08-23 23:03:31 +0200

    Fixed the error in the fusion plotting. Will extend this to SVM training as well

---
+++ experiments/fusion_mhd_lbp/get_score_pairs.m
@@ -0,0 +1,45 @@
+function [ genuines, imposters ] = get_score_pairs( d1, d2, num_per_sample, num_imposters, skip_diags )
+%UNTITLED Returns pair of genuine/imposter distances. This is important
+%when comparing scores to each other.
+% Rows are sorted according to col1, ie col1 has precedence
+    
+    addpath ../common
+
+    genuines = [];
+    imposters = [];
+    
+    for i = 1:size(d1,2)
+        
+        [start, finish] = get_limits(i, num_per_sample);
+
+        col1 = d1(:,i);
+        col2 = d2(:,i);
+        
+        % Making use of the fact that entries along diagonal are 0 when
+        % considering genuine distances
+        
+        if (skip_diags)
+            col1(i) = Inf;
+            col2(i) = Inf;
+        end
+        
+        distances = [col1 col2];
+        [dis, idx] = sortrows(distances);
+
+        correct_idx = idx(idx > start & idx <= finish);
+        corrects = distances(correct_idx,:);
+        corrects = sortrows(corrects);
+        gen = min(corrects);
+
+        imposter_idx = idx(idx <= start | idx > finish);
+        imp = distances(imposter_idx,:);
+        
+        genuines = [genuines ; gen];
+        imposters = [imposters ; imp(1:num_imposters,:)];
+    
+    end
+    
+    rmpath ../common
+
+end
+
--- experiments/fusion_mhd_lbp/plot_two.m
+++ experiments/fusion_mhd_lbp/plot_two.m
@@ -1,17 +1,23 @@
-start = 1; finish = 500;
+% start = 1; finish = 1;
 
-reduced_mhd = sort (imposter_mhd, 'ascend');
-reduced_mhd = reduced_mhd (start:finish,:); % Only keeps the lowest imposter scores for each sample
+% reduced_mhd = sort (imposter_mhd, 'ascend');
+% reduced_mhd = reduced_mhd (start:finish,:); % Only keeps the lowest imposter scores for each sample
+% 
+% reduced_lbp = sort(imposter_lbp_chi, 'ascend');
+% reduced_lbp = reduced_lbp (start:finish,:);
 
-reduced_lbp = sort(imposter_lbp, 'ascend');
-reduced_lbp = reduced_lbp (start:finish,:);
+num_per_sample = 6;
+num_imposters = 1;
+skip_diags = 1;
+[genuines, imposters] = get_score_pairs(distances, distances_ulbp_chi, num_per_sample, num_imposters, skip_diags);
 
 figure
-scatter (genuine_mhd, genuine_lbp, 'rx');
+scatter (genuines(:,1), genuines(:,2), 'rx');
 hold on
-scatter (reduced_mhd(:), reduced_lbp(:), 'g+');
+scatter (imposters(:,1), imposters(:,2), 'g+');
 
 xlabel ('Modified Hausdorff Distance between bifurcations');
 ylabel ('LBP using hamming distance');
 legend ('Genuine', 'Imposter');
 title ('Scores obtained from two feature extraction methods');
+
