5aeaefff3ccd5fe8a667f88c9b1a995546f42ef6
hmph <anurag.arnab@gmail.com>
2014-09-04 16:15:12 +0200

    Found an anti-aliasing script for figures. Makes plots look much better

--- experiments/centroid/clean/clusters_data.m
+++ experiments/centroid/clean/clusters_data.m
@@ -1,6 +1,6 @@
 id = 52;
 
-cl = train_cluster(6,100,0.9,veins,[0, 2, 3, 4, 1]);
+cl = train_cluster(6,100,50,veins,[0, 2, 3, 4, 1]);
 count = 0;
 test_thresh = 5;
 figure
@@ -37,6 +37,24 @@ cl(id).cluster = cl(id).cluster(~any(isnan(cl(id).cluster),2),:);
 [vx, vy] = voronoi ( cl(id).cluster(:,1), cl(id).cluster(:,2) );
 hold on
 plot (vx, vy, 'green');
+
+set (gca, 'XLim', [0 320]);
+set (gca, 'YLim', [0 80]);
+
+nice_green = [115, 174, 66]./255;
+nice_blue = [91, 155, 213]./255;
+nice_red = [192, 80, 77]./255;
+
+figure
+other_points = [veins(6*id).x veins(6*id).y];
+hold on
+scatter ( cl(id).cluster(:,1), cl(id).cluster(:,2), 72, nice_red, 'Marker', 'X', 'linewidth', 2);
+scatter (other_points(:,1), other_points(:,2), [], nice_blue, 'Marker', '+', 'linewidth', 2);
+plot (vx, vy, 'color', nice_green, 'linewidth', 2);
+goodplot();
+xlabel('X co-ordinate of bifurcation point');
+ylabel('Y co-ordinate of bifurcation point');
+legend({'Centroids of cluster','Bifurcation point of unseen image'}, 'Location', 'SouthOutside');
 
 set (gca, 'XLim', [0 320]);
 set (gca, 'YLim', [0 80]);
---
+++ experiments/centroid/clean/plotting_script.m
@@ -0,0 +1,4 @@
+[c, n] = distribution_tester(distances_centroid, 6, 0, 1);
+plot_pdf( c, n, 25, 200, ' ', 'Centroid Distance', 'Relative Frequency', {'Genuine scores','Imposter scores'});
+[corrects, negs] = tester(distances_centroid, imp_distances_centroid, [0:0.25:20], 1, 6);
+plot_roc(corrects, negs, [0:0.25:20], 13)
