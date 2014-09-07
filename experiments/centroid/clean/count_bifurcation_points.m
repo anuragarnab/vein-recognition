9adaf506f80c6d11d105759ff6c36e4628003796
hmph <anurag.arnab@gmail.com>
2014-09-07 08:16:24 +0200

    when saying what the optimal number of clusters are, this gives you an idea of how this relates to the size of all the other clusters

---
+++ experiments/centroid/clean/count_bifurcation_points.m
@@ -0,0 +1,24 @@
+num_per_sample = 6;
+num_bifs = zeros(length(veins)/num_per_sample,1);
+nice_blue = [0.3569 0.6078 0.8353];
+
+count = 0;
+for i = 1:length(veins)/num_per_sample
+    temp = 0;
+    for j = 1:6
+        count = count+1;
+        if (j == 6)
+            continue;
+        end
+        temp = temp + length(veins(count).x);
+        
+    end
+    num_bifs(i) = temp;
+end
+
+mean(num_bifs)
+figure
+hist(num_bifs, 20);
+set (findobj(gca,'Type','patch'), 'FaceColor', nice_blue)
+goodplot()
+myaa
