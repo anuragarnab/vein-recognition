f5d04db72ced62a4c3cb51b203f2901b44e8492f
hmph <anurag.arnab@gmail.com>
2014-09-14 10:00:44 +0200

    have svm tester training and testing for 1 fold. Need to automate the rest and then tune parameters

--- experiments/fusion_mhd_lbp/libsvm_test.m
+++ experiments/fusion_mhd_lbp/libsvm_test.m
@@ -2,7 +2,8 @@ num_per_sample = 6;
 num_imposters = 20;
 skip_diags = 1;
 testing = 3;
-zero_far = 709;
+zero_far = 709; % this is what I got from the accuracy graph
+all_predictions = [];
 
 [genuines, imposters] = get_score_pairs(distances_ulbp_chi, distances_mhd, num_per_sample, num_imposters, skip_diags);
 
@@ -34,12 +35,36 @@ labels(length(train_idx)+1:end) = -1;
 c =0.0000010; % default parameter
 gamma = 500;
 
-%fuck = svmtrain(labels,train_data,strcat({'-s 0 -t 0 -b 1 -c '}, num2str(c)  ) );
 svm_model = svmtrain(labels,train_data, '-s 0 -t 2 -b 1 -c 8 -g 0.5 -w1 10 -w-1 1');
 plotboundary(labels,train_data, svm_model, 't');
 
+%% Now the actual testing
+
+% I select the 100 best distances (selected by lbp) from the genuine data
+% I then select the 100 best imposter distances (one every sixth sample),
+% as chosen by lbp as well
+
+test_data = genuines(test_idx,:);
+
+imp_lbp = imp_distances_ulbp_chi';
+imp_mhd = imp_distances_mhd';
+test_imposters = zeros(size(imp_lbp,2) ,2);
+
+[~, idx] = min(imp_lbp);
+
+for i = 1:size(test_imposters,1)
+   test_imposters(i,1) = imp_lbp(idx(i),i);
+   test_imposters(i,2) = imp_mhd(idx(i),i);
+end
+
+test_data = [test_data ; test_imposters(test_idx,:)];
+test_data(:,1) = (test_data(:,1) - mu(:,1)) ./ sigma(1);
+test_data(:,2) = (test_data(:,2) - mu(:,2)) ./ sigma(2);
 
 
+expected = [ones(100,1) ; -ones(100,1)];
+[predictions, accuracy, scores] = svmpredict(expected, test_data, svm_model);
+all_predictions = [all_predictions predictions];
 
 
 
